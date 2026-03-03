5cf5737152c3c0472ccd1cbf9f41ab3b9896b0e0
MarkLeyer <91876236+MarkLeyer@users.noreply.github.com>
2026-03-03 13:07:05 +0100

    Update csi_top.v

--- firmware/hw_simple/RTL/csi_top.v
+++ firmware/hw_simple/RTL/csi_top.v
@@ -5,8 +5,10 @@ module csi_top #(
     parameter NUM_RAW = 8
 )(
     input  clk_ext,
+	
+	input [2:0] BUTTON,
 
-	output [7:0] LED
+	output reg [7:0] LED
     
 );
 
@@ -17,27 +19,42 @@ module csi_top #(
       .rst_ref_n (rst_ref_n)
     );
 	
-	localparam integer COUNT_MAX = 200 -1;
+	localparam integer COUNT_MAX = 500000 -1;
 	
 	reg [21:0] counter = 0;
 	reg [7:0] led_count = 0;
 
+	wire btn1;
+
+	CC_IBUF #(
+		.PULLUP (1)
+	) buttbuff (
+		.I (BUTTON[0]),
+		.Y (btn1)
+	);
+	
+
 always  @(posedge clk_ext)
 begin
-	if (!rst_ref_n) begin
-		counter <= 0;
-		led_count <= 0;
-		LED <= 8'b0;
-	end else begin
-		if (counter == COUNT_MAX) begin
+		if (counter >= COUNT_MAX) begin
 			counter <= 0;
 			led_count <= led_count + 1;
-			LED <= led_count + 1;
+			//LED <= led_count + 1;
 		end else begin
 			counter <= counter + 1;
 		end
-	end
+	LED <= led_count;
 end
+
+
+// always @(posedge clk_ext)
+// begin
+	// if (btn1 == 1'b1) begin
+		// LED <= 8'b0;
+	// end else begin
+		// LED <= 8'b1;
+	// end
+// end
 
 
   
