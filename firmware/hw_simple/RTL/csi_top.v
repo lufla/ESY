71ccaafdc77b23fd2ae95c93a4a3b93248465d7d
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2026-03-03 14:44:30 +0100

    Format

--- firmware/hw_simple/RTL/csi_top.v
+++ firmware/hw_simple/RTL/csi_top.v
@@ -5,11 +5,8 @@ module csi_top #(
     parameter NUM_RAW = 8
 )(
     input  clk_ext,
-	
-	input [2:0] BUTTON,
-
-	output reg [7:0] LED
-    
+    input [2:0] BUTTON,
+    output reg [7:0] LED
 );
 
     wire rst_ref_n;
@@ -18,45 +15,30 @@ module csi_top #(
       .clk_ref   (clk_ext),
       .rst_ref_n (rst_ref_n)
     );
-	
-	localparam integer COUNT_MAX = 500000 -1;
-	
-	reg [21:0] counter = 0;
-	reg [7:0] led_count = 0;
 
-	wire btn1;
+    localparam integer COUNT_MAX = 500000 - 1;
 
-	CC_IBUF #(
-		.PULLUP (1)
-	) buttbuff (
-		.I (BUTTON[0]),
-		.Y (btn1)
-	);
-	
+    reg [21:0] counter = 0;
+    reg [7:0] led_count = 0;
+
+    wire btn1;
+
+    CC_IBUF #(
+        .PULLUP (1)
+    ) buttbuff (
+        .I (BUTTON[0]),
+        .Y (btn1)
+    );
 
 always  @(posedge clk_ext)
 begin
-		if (counter >= COUNT_MAX) begin
-			counter <= 0;
-			led_count <= led_count + 1;
-			//LED <= led_count + 1;
-		end else begin
-			counter <= counter + 1;
-		end
-	LED <= led_count;
+    if (counter >= COUNT_MAX) begin
+        counter <= 0;
+        led_count <= led_count + 1;
+    end else begin
+        counter <= counter + 1;
+    end
+    LED <= led_count;
 end
-
-
-// always @(posedge clk_ext)
-// begin
-	// if (btn1 == 1'b1) begin
-		// LED <= 8'b0;
-	// end else begin
-		// LED <= 8'b1;
-	// end
-// end
-
-
-  
 
 endmodule
